655a6e49e30c79d5cc1041f3050fd326d0e61402
Tanish Singhal <tanishsinghal510@gmail.com>
2025-03-19 18:15:07 +0530

    Theme context

--- react-hooks-revision/src/App.jsx
+++ react-hooks-revision/src/App.jsx
@@ -3,15 +3,18 @@ import UseState from "./components/hooks/UseState";
 import UseEffect from "./components/hooks/UseEffect";
 import UseRef from "./components/hooks/UseRef";
 import UseContext from "./components/hooks/UseContext";
+import { ThemeProvider } from "./components/hooks/machine-ques/ThemeContext";
 
 function App() {
   return (
-    <div>
-      {/* <UseState /> */}
-      {/* <UseEffect /> */}
-      {/* <UseRef /> */}
-      <UseContext />
-    </div>
+    <ThemeProvider>
+      <div>
+        {/* <UseState /> */}
+        {/* <UseEffect /> */}
+        {/* <UseRef /> */}
+        {/* <UseContext /> */}
+      </div>
+    </ThemeProvider>
   );
 }
 
