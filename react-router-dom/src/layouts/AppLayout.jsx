ceafe08f142a0a27f61db75be3f01d2c3a7a5948
Tanish Singhal <tanishsinghal510@gmail.com>
2025-03-22 18:48:07 +0530

    useNavigation and useNavigate

--- react-router-dom/src/layouts/AppLayout.jsx
+++ react-router-dom/src/layouts/AppLayout.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { Outlet } from "react-router";
+import { Outlet, useNavigation } from "react-router";
 import Header from "../components/Header";
 
 const AppLayout = () => {
+  // usenavigation and usenavigate both are different things
+  const navigation = useNavigation();
+  // Go back button in comment is made by using usenavigate
+
+  const isLoading = navigation.state === "loading";
+
   return (
     <div>
       <Header />
-      {/* Laoding */}
+
+      {isLoading && <div>...Loading</div>}
+
       {/* Pages */}
 
       {/* outlet helps use to render all the pages */}
--- react-router-dom/src/pages/PostComments.jsx
+++ react-router-dom/src/pages/PostComments.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router";
 
 const PostComments = () => {
-  return (
-    <div>PostComments</div>
-  )
-}
+  const [loading, setLoading] = useState(true);
+  const [post, setPost] = useState(null);
+  const [comments, setComments] = useState([]);
 
-export default PostComments
+  const { postId } = useParams();
+
+  // usenavigation and usenavigate both are different things
+  const navigate = useNavigate();
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const [postResponse, commentsResponse] = await Promise.all([
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`),
+        axios.get(
+          `https://jsonplaceholder.typicode.com/posts/${postId}/comments`
+        ),
+      ]);
+
+      setPost(postResponse.data);
+      setComments(commentsResponse.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [postId]);
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          {post && (
+            <div key={post.id}>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          )}
+
+          <h2 style={{ marginTop: "50px" }}>Comments</h2>
+          {comments.map((comment) => (
+            <ul>
+              <li key={comment.id}>
+                <strong>{comment.name}</strong>: {comment.body}
+              </li>
+            </ul>
+          ))}
+        </div>
+      )}
+
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        Go Back
+      </button>
+    </div>
+  );
+};
+
+export default PostComments;
